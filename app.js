07e3af016703610e4d33608787b77ea02c4dd196
Pedro <pefernandez1408@gmail.com>
2020-06-01 13:27:08 -0430

    Conectando ambos servicios

--- app.js
+++ app.js
@@ -11,13 +11,28 @@ const argv = require('yargs').options({
 }).argv;
 
 //argv.direccion
-lugar.getLugarLatLng(argv.direccion)
+//lugar.getLugarLatLng(argv.direccion)
    //Una funcion async a fuerza regresa una promesa
-    .then(console.log);
+    //.then(console.log);
 
 
-    clima.getClima(9.910000, -68.309998)
-        .then(console.log)
-        .catch(console.log);
+    // clima.getClima(9.910000, -68.309998)
+    //     .then(console.log)
+    //     .catch(console.log);
         
+const getInfo = async (direccion) => {
 
+    try {
+        const coords = await lugar.getLugarLatLng( direccion );
+        const temp = await clima.getClima( coords.lat, coords.lng );
+        return `El clima de ${coords.direccion} es de ${temp}.`;
+    } catch (e) {
+        return `no se pudo determinar el clima de ${direccion}`;
+    }
+
+    
+}
+
+getInfo(argv.direccion)
+       .then(console.log)
+       .catch(console.log);
